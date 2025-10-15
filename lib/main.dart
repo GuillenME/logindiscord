21ce14dca197a64c6b5cf6197256cb92b4cb9940
rubendcdev <hugorubendc.developer@gmail.com>
2025-10-15 16:42:41 -0600

    fix(login): Reformatear codigo

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,23 @@ Import 'package:flutter/material.dart';
 import 'login_screen.dart';
 
 void main() {
-runApp(const MyApp());
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({Key? key}) : super(key: key);
+    const MyApp({Key? key}) : super(key: key);
 
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: 'Inicio de sesión',
-theme: ThemeData(
-primarySwatch: Colors.blue,
-),
-initialRoute: '/',
-routes: {
-'/': (context) => const LoginScreen(),
-},
-);
-}
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Inicio de sesión',
+            theme: ThemeData(
+                primarySwatch: Colors.blue,
+            ),
+            initialRoute: '/',
+            routes: {
+                '/': (context) => const LoginScreen(),
+            },
+        );
+    }
 }
